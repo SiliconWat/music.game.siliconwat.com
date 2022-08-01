7a0828ff56de74a68baaf30cc502f1360bb2d04d
Thon Ly <thonly@heartbank.ceo>
2022-08-01 15:31:47 -0700

    add remote components

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import { SwMenu } from "/components/sw-menu/element.mjs";
-customElements.define("sw-menu", SwMenu);
+const origin = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5508" : "https://music.siliconwat.com";
 
-import { SwEditor } from "/components/sw-editor/element.mjs";
-customElements.define("sw-editor", SwEditor);
+window.onload = async () => {
+    let module = await import(`${origin}/components/sw-menu/element.mjs`);
+    customElements.define("sw-menu", module.SwMenu);
 
-import { SwPiano } from "/components/sw-piano/element.mjs";
-customElements.define("sw-piano", SwPiano);
+    module = await import(`${origin}/components/sw-editor/element.mjs`);
+    customElements.define("sw-editor", module.SwEditor);
+
+    module = await import(`${origin}/components/sw-piano/element.mjs`);
+    customElements.define("sw-piano", module.SwPiano);
+}
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
